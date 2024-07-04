84e217b619ef3ad1ae5860b1ec2a4c7d242be4e4
axelinstrumental <aobscura@instrumental.net>
2024-07-04 09:50:46 -0600

    test2

--- src/app/api/generate/route.tsx
+++ src/app/api/generate/route.tsx
@@ -1,7 +1,9 @@
 import { OpenAIApi, Configuration } from "openai";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const GET = async (req: Request) => {
+export const POST = async (req: Request) => {
+
+  const data  =await req.json()
 
   const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -9,8 +11,8 @@ export const GET = async (req: Request) => {
   
   const openai = new OpenAIApi(config);
   
-  const topic = "concreto";
-  const keywords = "construcción con concreto";
+  const topic = data.tema;
+  const keywords = data.keywords;
   
   const postContentResult = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo-1106',
@@ -41,12 +43,11 @@ export const GET = async (req: Request) => {
 
   const postContent = postContentResult.data.choices[0]?.message?.content;
 
-  console.log(postContent);
-
   if (postContentResult.data && postContentResult.data.choices && postContentResult.data.choices[0]) {
     return Response.json({ message: postContent });
   } else {
     // Handle the case where data or choices are undefined
     return Response.json({ message: 'Data or choices are undefined' });
   }
+  
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,22 @@ import { Logo } from "./components/Logo";
 
 export default function Home() {
   const [articulo, setArticulo] = useState("");
-  const handleClick = async () => {
+  const [tema, setTema] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const nuevaConsulta = (e: any) => {
+    e.preventDefault();
+    setTema("");
+    setKeywords("");
+    setArticulo("");
+  };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
     const response = await fetch("/api/generate", {
-      method: "GET"
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ tema, keywords })
     });
     const data = await response.json();
     setArticulo(data.message);
@@ -17,25 +30,31 @@ export default function Home() {
       <div className="flex flex-col text-white overflow-hidden">
         <div className="bg-slate-800 px-2">
           <Logo />
-          <Link href="/post/new" className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block">
+          <button className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block" onClick={nuevaConsulta}>
             Nueva consulta
-          </Link>
+          </button>
         </div>
-        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
-          lista de posts
-        </div>
-        <div className="bg-cyan-800 flex items-center h-20">
-          Usuario
-        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800"></div>
+        <div className="bg-cyan-800 flex items-center h-20"></div>
       </div>
       <div className="bg-slate-900 flex flex-col items-center justify-center h-screen px-4">
-        <h1 className="text-white text-3xl text-upper font-bold mb-4">Generador de contenido sobre concreto</h1>
-        <div className="bg-slate-700 p-5 rounded-md w-full h-96 overflow-auto text-justify">
-          {articulo}
+        <h1 className="text-white text-3xl text-upper font-bold mb-4 uppercase font-thin">Generador de contenido sobre concreto</h1>
+        <div className="bg-slate-800 w-full block p-4 mb-3">
+          <form onSubmit={handleSubmit} className="w-full block p-4">
+            <label htmlFor="tema" className="text-white block mb-2">Tema:</label>
+            <input type="text" id="tema" name="tema" value={tema} onChange={(e) => setTema(e.target.value)} className="w-full px-2 py-1 rounded-md mb-4 text-slate-900 border-gray-100" />
+            <label htmlFor="keywords" className="text-white block mb-2">Palabras clave:</label>
+            <textarea id="keywords" name="keywords" value={keywords} onChange={(e) => setKeywords(e.target.value)} className="w-full px-2 py-1 rounded-md mb-4 text-slate-900" />
+            <button type="submit" className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 my-2 rounded-md hover:bg-green-600 transition-colors block">
+              GENERAR CONTENIDO
+            </button>
+          </form>
         </div>
-        <button className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block" onClick={handleClick}>
-          GENERAR CONTENIDO
-        </button>
+        <div className="bg-slate-700 p-5 rounded-md w-full h-96 overflow-auto text-justify">
+          <h3 className="text-white text-3xl w-full block pt-4 pb-0">{tema ? `Tema: ${tema}` : ''}</h3>
+          <h3 className="text-white text-3xl w-full block pt-0 pb-3 pb-4">{keywords ? `Palabras clave: ${keywords}` : ''}</h3>
+          <p className="font-thin text-2xl">{articulo}</p>
+        </div>
       </div>
     </div>
   );
