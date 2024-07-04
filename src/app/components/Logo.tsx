cc1516bb576d5e8b948ad95e0980fa277c7084bb
axelinstrumental <aobscura@instrumental.net>
2024-07-03 18:41:29 -0600

    auth

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,9 @@
+export const Logo = () => {
+  return (
+    <div className="bg-slate-800 px-2">
+      <div className="text-3xl text-center py-4 font-oswald">
+        GPT IMCYC
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import { Logo } from "./components/Logo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>INICIO</h2>
-      <h3>Inteligencia Artificial Para el Concreto y el Cemento</h3>
-      <Link href="/api/auth/login">
-        LOGIN
-      </Link>
-    </main>
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800 px-2">
+          <Logo />
+          <Link href="/post/new" className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block">
+            Nueva consulta
+          </Link>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          lista de posts
+        </div>
+        <div className="bg-cyan-800 flex items-center h-20">
+          Usuario
+        </div>
+      </div>
+      <div className="bg-slate-900">
+        Contenido
+      </div>
+    </div>
   );
 }
