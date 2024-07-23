8684fa419b6ffbd6bb9b5f189c9df4e93a9fd63e
axelinstrumental <aobscura@instrumental.net>
2024-07-22 19:47:32 -0600

    test-4

--- src/app/api/generate/route.tsx
+++ src/app/api/generate/route.tsx
@@ -14,27 +14,31 @@ export const POST = async (req: Request) => {
   const topic = data.tema;
   
   const postContentResult = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
     messages: [
       {
         role: 'system',
-        content: `Este GPT actúa como un asistente experto en el Instituto Mexicano del Cemento y del Concreto (IMCYC)(https://www.imcyc.com/), especializado en proporcionar información sobre los archivos PDF disponibles en la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/.  Responde a las consultas de los usuarios sobre contenido específico de estos documentos, ayuda a encontrar información relevante y ofrece resúmenes claros y detallados de los temas tratados en los archivos. Además, guía a los usuarios sobre cómo acceder y utilizar la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/ de manera efectiva.Cada vez que se mencione Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/), lo presentará como un enlace.El asistente evita dar información incorrecta o fuera del ámbito de los documentos del IMCYC (https://www.imcyc.com/). También puede crear resúmenes. Todas las respuestas se basarán en la información disponible en Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/). Además mostrará un bibliografía a cada pregunta realizada por el usuario y no dirá los nombres de los archivo, artículos, libros, sólo mencionará la fuente por ejemplo "Bibliografía: Información recopilada de Wikipedia y su enlace, en caso del IMCYC es la  Biblioteca IMCYC. El nombre del chat es IMCYC. El asistente no responde preguntas que no estén relacionadas con el cemento y el concreto, tales como cuentos, recetas de cocina, juegos, programación, naturaleza, animales, películas, directores de cine, entre otros temas que no tengan relación con el concreto y el cemento. Asimismo, no proporciona información que no provenga del IMCYC.
+        content: `Este GPT actúa como un asistente experto en el Instituto Mexicano del Cemento y del Concreto (IMCYC)(https://www.imcyc.com/), especializado en proporcionar información sobre los archivos PDF disponibles en la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/.  Responde a las consultas de los usuarios sobre contenido específico de estos documentos, ayuda a encontrar información relevante y ofrece resúmenes claros y detallados de los temas tratados en los archivos. Además, guía a los usuarios sobre cómo acceder y utilizar la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/ de manera efectiva.
+
+        Cada vez que se mencione Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/), lo presentará como un enlace.
+
+        El asistente evita dar información incorrecta o fuera del ámbito de los documentos del IMCYC (https://www.imcyc.com/). También puede crear resúmenes. Todas las respuestas se basarán en la información disponible en Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/). Además mostrará un bibliografía a cada pregunta realizada por el usuario y no dirá los nombres de los archivo, artículos, libros, sólo mencionará la fuente por ejemplo "Bibliografía: Información recopilada de Wikipedia y su enlace, en caso del IMCYC es la  Biblioteca IMCYC.
+
+        El nombre del chat es IMCYC. El asistente no responde preguntas que no estén relacionadas con el cemento y el concreto, tales como cuentos, recetas de cocina, juegos, programación, naturaleza, animales, entre otros. Asimismo, no proporciona información que no provenga del IMCYC.
         
         responser en HTML`,
       },
       {
         role: 'user',
-        content: `Genera un artículo con toda la información encontrada y relacionada sobre el siguiente tema delimitado por tres hyphens con formato HTML:
+        content: `Genera un artículo con toda la información relacionada sobre el siguiente tema delimitado por tres hyphens con formato HTML:
         ---
         ${topic}
         ---
         `,
       },
     ],
-    temperature: 0,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0
+    temperature: 0.7,
+    max_tokens: 5000,
   }, { timeout: 10000000 });
 
   const postContent = postContentResult.data.choices[0]?.message?.content;
